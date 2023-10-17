57858d44c472d08d1fe9d792ad44bd3be880daa4
Patrick Miller <paddymills@proton.me>
2023-10-17 16:44:29 -0400

    add timeout

--- src/nts.sh
+++ src/nts.sh
@@ -4,7 +4,7 @@ for number in {1..11}
 do
     echo "Sort $number"
     echo "------------"
-    # nameThatSort 0459 $number Y < input.txt
-    nameThatSort 0459 $number N < input.txt
+    # timeout 10s nameThatSort 0459 $number Y < input.txt
+    timeout 10s nameThatSort 0459 $number N < input.txt
     echo "==============================================="
 done
