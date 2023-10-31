e0122255e5ac4ada5fc737f3f3aa9f9cb5b68112
Patrick Miller <paddymills@proton.me>
2023-10-31 15:24:39 -0400

    add Chasing Oscars

---
+++ src/Solution.java
@@ -0,0 +1,17 @@
+
+/*
+ * Patrick Miller
+ * CMPSC 463
+ * Assignment #4: Chasing Oscars
+ */
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
