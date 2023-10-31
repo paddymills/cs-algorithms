01602f20ebce7c32db4d6dd7b54c3c24c3e876e5
Patrick Miller <paddymills@proton.me>
2023-10-31 15:48:56 -0400

    fix movie and rename

--- src/TopDown.java
+++ src/TopDown.java
@@ -6,41 +6,46 @@
  * Approach: Top-Down
  */
 
+package TopDown;
+
 
 import java.io.*;
 import java.util.*;
 
-public class TopDown {
+public class Solution {
 
     class Movie {
-        int id, startTime, endTime;
-        float ev;
+        int id;     // movie id
+        int start;  // start time
+        int end;    // time just after end (movie2.start == movie1.end is acceptable)
+        float ev;   // expected numbers of oscars to be won
 
         public Movie(String line) {
             String[] vals = line.split("\\s+");
-            int id = Integer.parseInt( vals[0] );
-            int startTime = Integer.parseInt( vals[1] );
-            int endTime = Integer.parseInt( vals[2] );
-            float ev = Float.parseFloat( vals[3] );
+
+            this.id    = Integer.parseInt( vals[0] );
+            this.start = Integer.parseInt( vals[1] );
+            this.end   = Integer.parseInt( vals[2] );
+            this.ev    = Float.parseFloat( vals[3] );
         }
     }
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        ArrayList<Movie> movies = readInput();
 
+        // impl
     }
 
-    public static Movie[] readInput() {
+    public static ArrayList<Movie> readInput() {
         // read input from stdin
         Scanner scanner = new Scanner(System.in);
 
         // first line: number of test cases
         int n = Integer.parseInt( scanner.nextLine() );
 
-        Movie[] movies = new Movie[n];
+        ArrayList<Movie> movies = new ArrayList();
         for (int i = 0; i < n; i++) {
-            // second line: values for n and k, respectively, space delimited
-            movies[i] = new Movie( scanner.nextLine() );
+            movies.add( new Movie(scanner.nextLine()) );
         }
 
         scanner.close();
