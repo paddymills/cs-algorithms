ad667009d90a12b5422e975b2707695d8130bd85
Patrick Miller <paddymills@proton.me>
2023-11-02 22:53:30 -0400

    case 1 bottomup success

--- src/Solution.java
+++ src/Solution.java
@@ -29,23 +29,23 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        ArrayList<Movie> movies = readInput();
+        Movie[]  movies = readInput();
 
-        for (Movie m : movies) {
-            System.out.println(m.id);
+        for (Integer id : bottomUp(movies)) {
+            System.out.println(id);
         }
     }
 
-    public static ArrayList<Movie> readInput() {
+    public static Movie[] readInput() {
         // read input from stdin
         Scanner scanner = new Scanner(System.in);
 
         // first line: number of test cases
         int n = Integer.parseInt( scanner.nextLine() );
 
-        ArrayList<Movie> movies = new ArrayList();
+        Movie[] movies = new Movie[n];
         for (int i = 0; i < n; i++) {
-            movies.add( new Movie(scanner.nextLine()) );
+            movies[i] = new Movie(scanner.nextLine());
         }
 
         scanner.close();
@@ -53,12 +53,90 @@ public class Solution {
         return movies;
     }
 
-    public static void topDown() {
-        // impl
+    public static ArrayList<Integer> topDown(Movie[] movies) {
+        Float[][] memo = new Float[movies.length+1][movies[movies.length-1].end];
+        boolean[][] taken = new boolean[movies.length+1][movies[movies.length-1].end];
+
+        topDownHelper(movies.length, movies[movies.length-1].end, movies, memo, taken);
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        // calculate result
+        int i = movies.length-1;
+        for (int t=movies[movies.length-1].end; t>0;) {
+            if ( taken[i][t] ) {
+                result.add(0, movies[i].id);
+                i--;
+            }
+        }
+
+        return result;
     }
 
-    public static void bottomUp() {
-        // impl
+    public static Float topDownHelper(int i, int t, Movie[] movies, Float[][] memo, boolean[][] taken) {
+        if ( t < 0 ) return Float.MIN_VALUE;
+        if ( t == 0 || i == 0 ) return 0f;
+
+        if ( memo[i][t] > 0 ) return memo[i][t];
+
+        // don't select movie
+        float notSelected = topDownHelper(i-1, t, movies, memo, taken);
+
+        // select movie
+        float selected = topDownHelper(i-1, movies[i].start, movies, memo, taken);
+
+        if ( selected > notSelected )
+            taken[i][t] = true;
+
+        memo[i][t] = Math.max(notSelected, selected);
+
+        return memo[i][t];
     }
-    
+
+    public static ArrayList<Integer> bottomUp(Movie[] movies) {
+        float[][] memo = new float[movies.length+1][movies[movies.length-1].end+1];
+        boolean[][] taken = new boolean[movies.length+1][movies[movies.length-1].end+1];
+
+        for (int i=1; i<memo.length; i++) {
+            // start at first end, since everything is 0's before then
+            for (int j=movies[0].end; j<memo[0].length; j++) {
+                float ev = movies[i-1].ev + memo[i-1][movies[i-1].start];
+                if ( movies[i-1].end <= j && ev > memo[i-1][j] ) {
+                    memo[i][j] = ev;
+                    taken[i][j] = true;
+                } else {
+                    memo[i][j] = memo[i-1][j];
+                }
+            }
+        }
+
+        printMemo(memo, taken);
+
+        // calculate result
+        ArrayList<Integer> result = new ArrayList<>();
+        int i = memo.length-1;
+        for (int j=memo[0].length-1; j>0;) {
+            if ( taken[i][j] ) {
+                result.add(0, movies[i-1].id);
+                j = movies[i-1].start;
+            }
+            i--;
+
+            if ( i < 0 )
+                break;
+        }
+
+        return result;
+    }
+
+    private static void printMemo(float[][] memo, boolean[][] taken) {
+        for (int i=0; i<memo.length; i++) {
+            System.err.print(i);
+            for (int j=0; j<memo[0].length; j++) {
+                String tstr = taken[i][j] ? "x" : "_";
+                System.out.print(" | " + memo[i][j] + tstr);
+            }
+            System.out.println();
+        }
+    }
 }
