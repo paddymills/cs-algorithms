de70443a3f1af7685ef0d5eb14294bdf2637ba5c
Patrick Miller <paddymills@proton.me>
2023-11-02 20:03:26 -0400

    cleanup nts

--- src/namethatsort/nts2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for runId in {1..3}
-do
-    echo "..$runId"
-    timeout 5m ./nameThatSort 0459 $1 "N" < input/$2_sorted.txt | python3 src/nts.py --upload input/$2_sorted.txt --sort-id $1
-    timeout 5m ./nameThatSort 0459 $1 "N" < input/$2_randomized.txt | python3 src/nts.py --upload input/$2_randomized.txt --sort-id $1
-done
--- src/namethatsort/nts3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-id=10
-for i in {1..4}
-do
-    timeout 5m ./nameThatSort 0459 $id "N" < $1 | python3 src/nts.py --upload $1 --sort-id $id
-done
