bd079c33061f2e8fe2cf200684681468063a06d1
Patrick Miller <paddymills@proton.me>
2023-11-28 20:23:26 -0500

    impl better shiftOne

--- src/eTSP.java
+++ src/eTSP.java
@@ -25,7 +25,7 @@ public class eTSP {
             this.y = Integer.parseInt( vals[1] );
         }
 
-        // euclidean distance from docs
+// euclidean distance from docs
         public double distanceTo(Point other) {
             return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
         }
@@ -37,12 +37,12 @@ public class eTSP {
     }
 
     static class Tour {
-        public int[] stops;
+        public ArrayList<Integer> stops;
         public double length;
         Point[] cities;
 
         public Tour(Point[] points) {
-            this.stops = new int[points.length];
+            this.stops = new ArrayList<>();
             this.cities = points;
         }
 
@@ -50,69 +50,60 @@ public class eTSP {
             initTour();
         }
 
-        /*
-         * init tour randomly
-        */
         void initTour() {
             HashSet<Integer> visitedCities = new HashSet<>();
-            
-            stops[0] = (int)(Math.random() * stops.length);
-            visitedCities.add(stops[0]);
 
-            for (int i=1; i<stops.length; i++) {
-                // progressPercentage(i, stops.length);
-                System.out.print("\r" + i);
+            stops.add((int) (Math.random() * stops.size()));
+            visitedCities.add(stops.get(0));
 
+            for (int i = 1; i < cities.length; i++) {
                 int id;
                 do {
-                    id = (int)(Math.random() * stops.length);
+                    id = (int) (Math.random() * cities.length);
                 } while (visitedCities.contains(id));
 
-                // commit closest city
                 visitedCities.add(id);
-                stops[i] = id;
-                length += cities[stops[i-1]].distanceTo(cities[stops[i]]);
+                stops.add(id);
+                length += cities[stops.get(i - 1)].distanceTo(cities[stops.get(i)]);
             }
 
-            // cleanup stdout
-            System.out.print("\r");
-
-            // add distance from first stop to last stop
-            this.length += cities[stops[0]].distanceTo(cities[stops[stops.length-1]]);
+            this.length += cities[stops.get(0)].distanceTo(cities[stops.get(stops.size() - 1)]);
         }
 
         public boolean shiftOneStop() {
-            ArrayList<Integer> tmpTour = new ArrayList<>();
             boolean newTour = false;
-            
+
             for (int i = 0; i < cities.length; i++) {
                 for (int j = 0; j < cities.length; j++) {
-                    // move stop[i] to stop[j]
+                    // move stops[i] to stops[j]
+                    if (i == j) continue;
+                    
+                    // calculate move
+                    double score = length;
+                    // remove end if i or j are at ends
+                    if (i == 0 || i == stops.size()-1 || j == 0 || j == stops.size()-1)
+                        score -= cities[stops.get(0)].distanceTo(cities[stops.get(stops.size() - 1)]);
 
-                    tmpTour.clear();
+                    // subtract current node i distances
+                    if (i > 0)  // remove i.left
+                        score -= cities[stops.get(i-1)].distanceTo(cities[stops.get(i)]);
+                    if (i < cities.length-1)    // remove i.right
+                        score -= cities[stops.get(i)].distanceTo(cities[stops.get(i+1)]);
 
-                    for (int k = 0; k < j; k++) {
-                        System.out.print("\r<"+i+","+j+","+k+">");
-                        if (k != i)
-                            tmpTour.add(stops[k]);
-                    }
-                    tmpTour.add(stops[i]);
-                    for (int k = j; k < stops.length; k++) {
-                        System.out.print("\r<"+i+","+j+","+k+">");
-                        if (k != i)
-                            tmpTour.add(stops[k]);
-                    }
+                    // subtract current j node distances
+                    if (j > 0 && j - 1 != i )  // remove j.left, if was not i
+                        score -= cities[stops.get(j-1)].distanceTo(cities[stops.get(j)]);
 
-                    double score = 0;
-                    for (int k = 1; k < stops.length; k++) {
-                        score += cities[tmpTour.get(k-1)].distanceTo(cities[tmpTour.get(k)]);
-                    }
-                    score += cities[tmpTour.get(0)].distanceTo(cities[tmpTour.get(tmpTour.size()-1)]);
-
+                    // add (i).distanceTo(j-1)
+                    // add (i).distanceTo(j)
+                    // add end if needed (part of last 2?)
+                    
                     if (score < length) {
-                        // commit new
-                        for(int k=0; k<tmpTour.size(); k++)
-                            stops[k] = tmpTour.get(k);
+                        // move items
+                        Integer elem = stops.get(i);
+                        stops.remove(elem);
+                        stops.add(j, elem);
+
                         length = score;
 
                         newTour = true;
@@ -139,21 +130,21 @@ public class eTSP {
 
         for (int i = 0; i < 5; i++) {
             System.out.println(String.format("%.07f", tour.length));
-            // for (int stop : tour.stops) {
+// for (int stop : tour.stops) {
             //     System.out.println(points[stop].toString());
             // }
     
             System.out.println("Your grade is: " + String.format("%.02f%%", grade(tour.length)));
-            
+
             System.out.println("+++++++++++++++++++++");
             if (!tour.shiftOneStop()) break;
         }
     }
 
     public static Point[] readInput() {
-        // read input from stdin
+// read input from stdin
         Scanner scanner = new Scanner(System.in);
-
+        
         // first line: number of test cases
         int n = Integer.parseInt( scanner.nextLine() );
 
@@ -167,7 +158,7 @@ public class eTSP {
         return points;
     }
 
-    /*
+/*
      * Grade calculation from rubric
      * 
      * grade = -1.009 * 10^-5 * tourLength + 224
